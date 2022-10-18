54d60e78cf0dad6df466188d6642905978a8f312
Nichkov Roman <nichkovroman@gmail.com>
2022-10-18 18:08:41 +0300

    feat: add section pickup

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,46 @@
 import * as customFunctions from './modules/functions.js'
 
 // vendors
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+// printing eqv swiper
+const swiper1 = new Swiper('.swiper--printing', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    modules: [Navigation, Pagination],
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+// postprinting eqv swiper
+const swiper2 = new Swiper('.swiper--postprinting', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    modules: [Navigation, Pagination],
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
