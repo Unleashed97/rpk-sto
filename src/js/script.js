27262494c9b2c0fa7ae387e47e9e8cbecdec0181
Nichkov Roman <nichkovroman@gmail.com>
2022-10-19 08:15:51 +0300

    feat: add works swiper

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,24 @@ const swiper2 = new Swiper('.swiper--postprinting', {
         prevEl: '.swiper-button-prev',
     },
 })
+
+// postprinting eqv swiper
+const swiper3 = new Swiper('.swiper--works', {
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    modules: [Navigation, Pagination],
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
